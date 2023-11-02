42f90766e92119e7179bef643a970096dabab3fb
Ryan Plant <ryan@ryanplant.net>
2023-11-02 08:16:09 +0100

    Add bookmark

---
+++ bookmark.js
@@ -0,0 +1,12 @@
+function bookmark() {
+  var time_pos = Math.floor(mp.get_property_number("time-pos"));
+  if (time_pos > 5) time_pos -= 10;
+  var path = mp.get_property("path");
+  var valueBytes = path.length;
+  var formattedPath = "%" + valueBytes + "%" + path;
+  var s = formattedPath + "," + time_pos + "," + 40 + ",oshash=null" + "\n";
+  mp.utils.append_file("file://~/vids.edl", s);
+  mp.osd_message("Bookmarked!");
+}
+
+mp.add_key_binding("b", "bookmark", bookmark);
